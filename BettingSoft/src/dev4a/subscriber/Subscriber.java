b54f2ae04e101289682b4e68f5d5798038cc34f5
Ahmed SAMI MOHAMED <asyn.nile@gmail.com>
2016-05-18 11:50:47 +0200

    Added Id to Subscriber

--- BettingSoft/src/dev4a/subscriber/Subscriber.java
+++ BettingSoft/src/dev4a/subscriber/Subscriber.java
@@ -14,36 +14,54 @@ import dev4a.bets.*;
  */
 public class Subscriber {
 	
+	/* The id of the Subscriber 
+	 * Integer
+	 */
+	private int id;
+
 	/* The last name of the Subscriber 
 	 * UTF-8 String 
 	 */
 	private String lastName;
+
 	/* The first name of the Subscriber 
 	 * UTF-8 String 
 	 */
 	private String firstName;
+
 	/* The username of the Subscriber 
 	 * UTF-8 String used to identify him UNIQUELY
 	 */
 	private String userName;
+
 	/* The password of the Subscriber 
 	 * UTF-8 String TODO HASH this!!
 	 */
 	private String password = "";
+
 	/* The date of birth of the Subscriber 
 	 * UTF-8 String ! Serves for checking age restrictions
 	 */
 	private String bornDate;
+
 	/* The amount in the account of the Subscriber 
 	 * a long integer ( he can be really rich if he bets right )
 	 */
 	private long numberOfTokens;
+
 	/* Constructors of this class */
 	public Subscriber(){
 		/*
 		 * Empty constructor for Hibernate
 		 */
 	}
+
+	public Subscriber( int id, String lastName, String firstName, String userName, String bornDate) {
+		/* the constructor with all params */
+		this.id = id;
+		this(lastName, firstName, userName, bornDate);
+	}
+
 	public Subscriber( String lastName, String firstName, String userName, String bornDate) {
 		/* the constructor with all params */
 		this.lastName = lastName;
@@ -80,7 +98,16 @@ public class Subscriber {
 		setPassword(newPassword);
 		return true;
 	}
+
 	/* getters and setters REQUIRED for the POJO  (hibernate as well) */
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getLastName() {
 		return lastName;
 	}
@@ -132,6 +159,7 @@ public class Subscriber {
 	
 	/* cancel a bet */
 	public long cancelBet(Bet betToCancel) {
+
 		return 0l;
 	}
 	
